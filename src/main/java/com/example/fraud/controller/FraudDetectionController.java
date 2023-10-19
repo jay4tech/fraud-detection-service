cf93aba53e4ccc4b9653a0c86a0c2c4d2588c89f
jay4tech <jayprakashatcgc@gmail.com>
2023-10-19 07:51:48 +0530

    Comprehensive Unguided Scenario-based Exercise - Spring Microservice Design Patterns - Online Banking System #173

--- src/main/java/com/example/fraud/controller/FraudDetectionController.java
+++ src/main/java/com/example/fraud/controller/FraudDetectionController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("fraudDetection")
 public class FraudDetectionController {
 
-
     @Autowired
     IFraudDetectionService fraudDetectionService;
     @PostMapping("/analyzeTransaction")
